93e95af076a6eaf17cf3ad4458d5f9d6457b907d
Prajwal B S <legendamin008@gmail.com>
2023-04-18 20:24:40 +0530

    my first project on Go

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+import (
+	"fmt"
+
+
+)
+
+func main(){
+	var confna="Go Conferenece"
+	const tickets=50
+	var remaintickets=50
+	var name string
+	var utickets int
+
+fmt.Println("GO Project!")
+fmt.Printf("Welcome to %s!\n",confna)
+fmt.Printf("Get Your Tickets There are %d Tickets Availabel\n",remaintickets)
+fmt.Println("Enter Your Name")
+fmt.Scan(&name) 
+fmt.Println("Enter the no.of tickets")
+fmt.Scan(&utickets)
+fmt.Printf("%v booked %v tickets",name,utickets)
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
