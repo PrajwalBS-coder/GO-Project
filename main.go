348bea47041753978b19247a4085979b185aab54
Prajwal B S <legendamin008@gmail.com>
2023-04-19 14:19:22 +0530

    more lines of code

--- main.go
+++ main.go
@@ -8,22 +8,32 @@ import (
 func main(){
 	var confna="Go Conferenece"
 	const tickets int=50
-	var remaintickets int =50
+	var remaintickets uint =50
 	var name,email string
-	var utickets int
+	var utickets uint
+	var bookings [50]string
 
 fmt.Println("GO Project!")
 fmt.Printf("Welcome to %s!\n",confna)
 fmt.Printf("Get Your Tickets There are %d Tickets Availabel\n",remaintickets)
+
+
+
+
+
 fmt.Println("Enter Your Name")
 fmt.Scan(&name)
 fmt.Println("Enter Your Email")
 fmt.Scan(&email) 
 fmt.Println("Enter the no.of tickets")
 fmt.Scan(&utickets)
+remaintickets=remaintickets-utickets
 fmt.Printf("Thank You %v for booking %v tickets you willl confirmation email to this %v email ",name,utickets,email)
+fmt.Printf("\n%v tickets remaining for %v",remaintickets,confna)
 
-
+bookings[0]=name
+fmt.Printf("array values %v",bookings)
+fmt.Printf("array values %v",bookings[0])
 
 
 
