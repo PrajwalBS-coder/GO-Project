713f408353e3f5cedfeaf3771166e6065e106bab
Prajwal B S <legendamin008@gmail.com>
2023-06-19 19:32:45 +0530

    added more lines of code

--- main.go
+++ main.go
@@ -1,48 +1,46 @@
 package main
+
 import (
 	"fmt"
-
-
+	"strings"
 )
 
-func main(){
-	var confna="Go Conferenece"
-	const tickets int=50
-	var remaintickets uint =50
-	var name,email string
+func main() {
+	var confna = "Go Conferenece"
+	const tickets int = 50
+	var remaintickets uint = 50
+	var fname, lname, email string
 	var utickets uint
-	var bookings [50]string
+	bookings := []string{}
 
-fmt.Println("GO Project!")
-fmt.Printf("Welcome to %s!\n",confna)
-fmt.Printf("Get Your Tickets There are %d Tickets Availabel\n",remaintickets)
+	fmt.Println("GO Project!")
+	fmt.Printf("Welcome to %s!\n", confna)
+	fmt.Printf("Get Your Tickets There are %d Tickets Availabel\n", remaintickets)
 
+	//taking inputs from user
+	for {
+		fmt.Println("Enter Your Name")
+		fmt.Scan(&fname)
+		fmt.Println("Enter Your Name")
+		fmt.Scan(&lname)
+		fmt.Println("Enter Your Email")
+		fmt.Scan(&email)
+		fmt.Println("Enter the no.of tickets")
+		fmt.Scan(&utickets)
+		remaintickets = remaintickets - utickets
+		bookings = append(bookings, fname+" "+lname)
+		fmt.Printf("Thank You %v%v for booking %v no.of tickets you willl confirmation email to this %v email ", fname, lname, utickets, email)
+		fmt.Printf("\n%v tickets remaining for %v", remaintickets, confna)
 
+		fn := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			fn = append(fn, names[0])
 
+		}
 
+		fmt.Printf("\nAll the Bookings thart are Done %v\n", fn)
 
-fmt.Println("Enter Your Name")
-fmt.Scan(&name)
-fmt.Println("Enter Your Email")
-fmt.Scan(&email) 
-fmt.Println("Enter the no.of tickets")
-fmt.Scan(&utickets)
-remaintickets=remaintickets-utickets
-fmt.Printf("Thank You %v for booking %v no.of tickets you willl confirmation email to this %v email ",name,utickets,email)
-fmt.Printf("\n%v tickets remaining for %v",remaintickets,confna)
-
-bookings[0]=name
-fmt.Printf("array values %v",bookings)
-fmt.Printf("array values %v",bookings[0])
-
-
-
-
-
-
-
-
-
-
+	}
 
 }
