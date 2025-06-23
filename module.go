0204a1e2cae978181b4bb5491f8e318e5d9b0a81
Prajwal B S <prajwaljhon@gmail.com>
2025-06-23 20:40:52 +0530

    Modules

--- module.go
+++ module.go
@@ -1,7 +1,14 @@
-package module
-import(
+package main
+
+import (
 	"fmt"
+"gonum.org/v1/gonum/stat"
 )
-func add(a,b)int{
-	return a+b
+
+func main(){
+	l:=[] float64{1, 2, 3, 4, 5}
+	mean := stat.Mean(l, nil)
+	fmt.Println("Mean:", mean)
+	median := stat.Quantile(0.5, stat.Empirical, l, nil)
+	fmt.Println("Median:", median)
 }
